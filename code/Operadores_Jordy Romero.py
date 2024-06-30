46de3d4a603871076066e9a294c91670636b6d01
Jordy-Romero <jromero.11@hotmail.com>
2024-06-29 20:47:14 -0500

    Add files via upload

---
+++ code/Operadores_Jordy Romero.py
@@ -0,0 +1,51 @@
+"""Operadores en Python"""
+"""Operadores aritméticos"""
+a = 2
+b = 3
+suma = a + b #esto es una suma
+print('La suma de: ', a,'más', b,'es: ', suma)
+resta = a - b #esto es una resta
+print('La resta de: ', a,'menos', b,'es: ', resta)
+multiplicacion = a * b #esto es una multiplicacion
+print('la multiplicacion de: ', a,'multiplicado por', b,'es: ', multiplicacion)
+division = a / b #esto es una division
+print('la division de: ', a,'divido para', b,'es: ', division)
+division_entera = a // b #esto es una division entera
+print('la division entera de: ', a,'divido para', b,'es: ', division_entera)
+potenciacion = a ** b #esto es una potenciacion
+print('la potenciacion de: ', a,' elevado a', b,'es: ', potenciacion)
+
+#Operadores Relacionales
+a = 6
+b = 7
+c = 2 
+d = 1
+print(a, 'es menor que', b, a < b)
+
+#Operadores Lógicos
+p= 5 > 2
+q= 2 < 1
+p or q
+p and q
+not p or q
+not p and q
+print('la proposición p o q es: ', p or q)
+print('la proposicón p y q es: ',p and q)
+print('la proposición not p o q es: ', not p or q)
+print('la proposicón not p y q es: ',not p and q)
+
+"""TIpos de divisiones"""
+#Entera:
+27//14
+#Decimal
+27/14
+#Módulo
+27%14
+
+
+
+
+
+
+
+
